a533050d7205e1b87e253fce93e29fcad47b7d30
Chunyu Ju <66151484+JackJu-HIT@users.noreply.github.com>
2022-12-11 18:34:54 +0800

    Update RANSAC.cpp

--- src/RANSAC.cpp
+++ src/RANSAC.cpp
@@ -6,7 +6,7 @@ int main()
 {  
 	GradDecent gradSolve;
 	vector<vector<double> > Sample = { {1,11},{2,13 }, { 3, 15 }, { 4, 17 } , {5,19},{6,21} };
-    int time = 0;
+        int time = 0;
 	double j = 0;
 	/*while(time < 100){
 		vector<double> temp;
@@ -18,8 +18,8 @@ int main()
 		temp.clear();
 	}*/
 	
-    gradSolve.sampleData = Sample;
-    vector<double> re = gradSolve.calculateGradDescent();
+        gradSolve.sampleData = Sample;
+        vector<double> re = gradSolve.calculateGradDescent();
 	for(int i = 0;i < re.size();i++){
 			cout<<re[i]<<endl;
 	}
@@ -29,11 +29,5 @@ int main()
 	MATRIXINVERSE matrixinverse;
 	//matrixinverse.m_r = 2;
 	cout<<matrixinverse.calculateMATRIXInverse()<<endl;
-	/*
-	circle c1;//c1是起得名
-	//给圆的对象的属性进行赋值
-	c1.m_r=10;
-	cout<<"圆的周长（改）："<<c1.calculate()<<endl;;
-	*/
 
 }
